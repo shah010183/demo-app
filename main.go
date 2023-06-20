447d736676f12fed79be7c956dea86a5aa03b0fc
shah010183 <shah010183@gmail.com>
2023-06-20 14:36:26 -0700

    adding one more ! to comment to test notification changes

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	myFigure := figure.NewColorFigure("shah010183-1 is Awesome!!!!", "larry3d", "yellow", true)
+	myFigure := figure.NewColorFigure("shah010183-1 is Awesome!!!!!", "larry3d", "yellow", true)
 	myFigure.Print()
 
 	if secret := os.Getenv("SECRET"); secret != "" {
